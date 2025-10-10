186281fa573bd0727490c0fb6fca5d525798b4a8
AJ <>
2025-10-10 18:08:52 +0300

    [ADDED] show tags on details screen

--- app/src/main/java/com/example/myapplication/imagedetails/ui/PlantDetailDescription.kt
+++ app/src/main/java/com/example/myapplication/imagedetails/ui/PlantDetailDescription.kt
@@ -8,30 +8,58 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.R
+import com.example.myapplication.app.ui.UiState
+import com.example.myapplication.pixabay.domain.PixabayImage
+import timber.log.Timber
 
 @Composable
 fun PlantDetailDescription(viewModel: ImageDetailsViewModel) {
 
-    val currentImage = viewModel.currentImage.collectAsState()
+    val currentState by viewModel.currentImage.collectAsState()
 
-    Surface {
-        Text(
-            text = "Hello Compose",
-            modifier = Modifier
-                .padding(horizontal = dimensionResource(R.dimen.margin_small))
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.CenterHorizontally),
-            style = MaterialTheme.typography.headlineMedium
-        )
+    when (currentState) {
+        is UiState.Error -> Timber.d("Got error ${(currentState as UiState.Error).message}")
+        is UiState.Loading -> Timber.d("Loading")
+        is UiState.Success -> {
+            val image = (currentState as UiState.Success).data
+            Timber.d("Got data $image")
+            PlantDetailDescription(image)
+        }
     }
 }
 
-//@Preview
-//@Composable
-//fun PlantDetailDescriptionPreview() {
-//    PlantDetailDescriptio
-//}
+@Composable
+private fun PlantDetailDescription(image: PixabayImage) {
+    Surface {
+        image.largeImageUrl?.let {
+        }
+
+        PixableImageTags(image.tags)
+    }
+}
+
+@Composable
+fun PixableImageTags(tags: List<String>) {
+    Text(
+        text = tags.joinToString(", "),
+        modifier = Modifier
+            .padding(horizontal = dimensionResource(R.dimen.margin_small))
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally),
+        style = MaterialTheme.typography.headlineMedium
+    )
+}
+
+@Preview
+@Composable
+fun PlantDetailDescriptionPreview() {
+    MaterialTheme {
+        PixableImageTags(listOf("tag1", "tag2", "tag3") )
+    }
+}
